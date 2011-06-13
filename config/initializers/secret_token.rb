56530538aff836be0fdb13b73ee87d404a58db8f
kmkkmk <kmkkmk@web.de>
2011-06-13 21:50:58 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MemoApp::Application.config.secret_token = '7d7a955cd1b8dfcd0fbc7634b78293a7a80c8303c16eba0968c4668ab822a8e060aff13744204c725b9b962fa5465af677cf832d42bfffaf32631775c175d1b7'
